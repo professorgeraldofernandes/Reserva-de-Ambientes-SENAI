8f7c8a81241870eecd424ccd50596571f6c4fb55
Geraldo José Fernandes <75139615+professorgeraldofernandes@users.noreply.github.com>
2026-07-24 20:31:33 -0300

    feat: adiciona estrutura inicial de dados importados da planilha

---
+++ src/imported-data.js
@@ -0,0 +1,3 @@
+export const importedReservationRows = [["2026-01-06","r1-12-sala","noite","completo","Marco Valerio","SALAS R1"],["2026-01-08","r1-12-sala","noite","completo","FERNANDO TEC. MEC.","SALAS R1"],["2026-01-13","r1-12-sala","noite","completo","Marco Valerio","SALAS R1"],["2026-01-15","r1-12-sala","noite","completo","FERNANDO TEC. MEC.","SALAS R1"]];
+
+export const importedInstitutionalEvents = [];
